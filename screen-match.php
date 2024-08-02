7d35f8d94c41d8bfc3e957ac21d97b50f39dd450
marcospaulopaiva <marcos.poo@gmail.com>
2024-08-01 21:27:36 -0300

    Aula 2.3 - Trabalhando com textos

--- screen-match.php
+++ screen-match.php
@@ -7,6 +7,8 @@ $nomeFilme = "Top Gun - Maverick";
 $anoLancamento = 2022;
 $somaDeNotas = 9 + 6 + 8 + 7.5 + 5;
 $notaFilme = $somaDeNotas / 5;
-$incluidoNoPlano = true;
+$planoPrime = true;
+$incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
 
-echo $notaFilme;
+echo "Nome do filme: " . $nomeFilme . "\n";
+echo "Nota do filme: $notaFilme\n";
