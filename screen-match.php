9dee927930e551590576eb31dd2efa4ce8b36c1f
marcospaulopaiva <marcos.poo@gmail.com>
2024-08-01 23:59:54 -0300

    Aula 3.1 - Tomando decisões

--- screen-match.php
+++ screen-match.php
@@ -5,6 +5,7 @@ echo "Bem-vindo(a) ao screen match!
 
 $nomeFilme = "Top Gun - Maverick";
 $anoLancamento = $argv[1] ?? 1991;
+
 $somaDeNotas = 9 + 6 + 8 + 7.5 + 5;
 $notaFilme = $somaDeNotas / 5;
 $planoPrime = true;
@@ -13,3 +14,14 @@ $incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
 echo "Nome do filme: " . $nomeFilme . "\n";
 echo "Nota do filme: $notaFilme\n";
 echo "Ano de lançamento: $anoLancamento\n";  
+
+
+if($anoLancamento > 2022){
+    echo "Esse filme é um lançamento \n";
+} 
+elseif($anoLancamento > 2020 && $anoLancamento <= 2022){
+    echo "Esse filme ainda é novo \n";
+}
+else {
+    echo "Esse filme NÃO é um lançamento \n";
+}
